24a4d6195bfcb54000829cf12e7f37b5e8e9bcfc
Alfredo Suárez Romero <48132178+sibokdev@users.noreply.github.com>
2024-05-10 10:15:22 -0600

    Added basic security and keycloak integration

--- src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -1,14 +1,21 @@
 package com.polarbookshop.edgeservice.config;
-
+import java.security.Principal;
 import reactor.core.publisher.Mono;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RateLimiterConfig {
-    @Bean
+    /*@Bean
     public KeyResolver keyResolver() {
         return exchange -> Mono.just("anonymous");
+    }*/
+
+    @Bean
+    KeyResolver keyResolver() {
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
     }
 }
 
---
+++ src/main/java/com/polarbookshop/edgeservice/web/UserController.java
@@ -0,0 +1,27 @@
+package com.polarbookshop.edgeservice.web;
+
+import java.util.List;
+
+import com.polarbookshop.edgeservice.user.User;
+import reactor.core.publisher.Mono;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("user")
+    public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        var user = new User(
+                oidcUser.getPreferredUsername(),
+                oidcUser.getGivenName(),
+                oidcUser.getFamilyName(),
+                List.of("employee", "customer")
+        );
+        return Mono.just(user);
+    }
+
+}
